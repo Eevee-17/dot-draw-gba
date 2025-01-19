6cb2b05af4ebe119c9cc470c5ac339d5e6b244aa
Eevee 17 <99231914+Eevee-17@users.noreply.github.com>
2025-01-19 13:03:47 -0600

    Optimized dotdraw.asm more

--- dotdraw.asm
+++ dotdraw.asm
@@ -1,33 +1,28 @@
-@define BLACK %0000000000000000 ; -bbbbbgggggrrrrr
-@define WHITE %0111111111111111 ; "
+@define WHITE %0111111111111111 ; -bbbbbgggggrrrrr
 
 
 b start
 @include header.h
 
-
-COLBLACK
-@dcw BLACK, BLACK
-
-COLWHITE
+COLWHITE:
 @dcw WHITE, WHITE
 
 
 start:
-ldr r0,=0x04000000 ; I/O
+mov r0,0x04000000 ; I/O
 
-ldr r1,=%0000010000000011 ; turn on BG2, set BG mode to 3
+@dcd 0xE51F1078; ldr r1,=%0000010000000011 ; turn on BG2, set BG mode to 3 ; Optimization note: I need to set r1 to 0x0403. I got lucky, and 0x0403 can be found in the header in a good spot for the GBA to access it.
 str r1,[r0]
 
 ldr r1,[COLWHITE]
 str r1,[r0,0xD4] ; DMA 3 source address
 
-ldr r1,=0x06000000 ; VRAM
+mov r1,0x06000000 ; VRAM
 str r1,[r0,0xD8] ; DMA 3 destination address
 
-ldr r2,[COLBLACK] ; initial dot color
-ldr r3,=240 ; initial x-pos represented by x = x-pos px * 2
-ldr r4,=38400 ; initial y-pos represented by y = y-pos px * 240 * 2
+; Optimization note: r2 is zero-filled on start, so I don't need to set it to zero.
+mov r3,0xF0; 0xF0 = 240 ; initial x-pos represented by x = x-pos px * 2
+mov r4,0x9600; 0x9600 = 38400 ; initial y-pos represented by y = y-pos px * 240 * 2
 
 clearScreen:
 ldr r5,=(0x85000000|19200) ; 0x85000000 (check binary): enable DMA, set 32-bit transfer type, and set as fixed source. 19200: 240px * 160px / 2
@@ -37,11 +32,11 @@ b drawDot
 
 mainLoop:
 ; wait for VBlank
-waitVBEnd
+waitVBEnd:
 ldrh r5,[r0,0x06] ; V counter
 cmp r5,0
 bne waitVBEnd
-waitVBStart
+waitVBStart:
 ldrh r5,[r0,0x06] ; "
 cmp r5,160
 bne waitVBStart
@@ -53,12 +48,12 @@ cmp r4,r6
 tstle r5,0x80 ; test for D-pad Down pressed
 addeq r4,r4,480 ; 480 = 1px * 240 * 2
 
-ldr r6,=480 ; 480 = 1px * 240 * 2
+mov r6,0x1E0; 0x1E0 = 480 ; 480 = 1px * 240 * 2
 cmp r4,r6
 tstge r5,0x40 ; test for D-pad Up pressed
 subeq r4,r4,480 ; 480 = 1px * 240 * 2
 
-ldr r6,=2 ; 2 = 1px * 2
+mov r6,0x2 ; 2 = 1px * 2
 cmp r3,r6
 tstge r5,0x20 ; test for D-pad Left pressed
 subeq r3,r3,2 ; 2 = 1px * 2
@@ -69,7 +64,7 @@ tstle r5,0x10 ; test for D-pad Right pressed
 addeq r3,r3,2 ; 2 = 1px * 2
 
 tst r5,0x01 ; test for A pressed
-ldr r2,[COLBLACK]
+mov r2,0x0000 ; set r2 to the color black
 ldreq r2,[COLWHITE]
 
 tst r5,0x02 ; test for B pressed
