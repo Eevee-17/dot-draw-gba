248875c1740a2205a804d0f985e8ca5fe364e580
Eevee 17 <99231914+Eevee-17@users.noreply.github.com>
2025-01-19 21:41:14 -0600

    Cleaned up dotdraw.asm
    Added pound signs before decimal numbers

--- dotdraw.asm
+++ dotdraw.asm
@@ -25,7 +25,7 @@ mov r3,0xF0; 0xF0 = 240 ; initial x-pos represented by x = x-pos px * 2
 mov r4,0x9600; 0x9600 = 38400 ; initial y-pos represented by y = y-pos px * 240 * 2
 
 clearScreen:
-ldr r5,=(0x85000000|19200) ; 0x85000000 (check binary): enable DMA, set 32-bit transfer type, and set as fixed source. 19200: 240px * 160px / 2
+ldr r5,=(0x85000000|#19200) ; 0x85000000 (check binary): enable DMA, set 32-bit transfer type, and set as fixed source. 19200: 240px * 160px / 2
 str r5,[r0,0xDC] ; DMA 3 control
 
 b drawDot
@@ -34,34 +34,34 @@ mainLoop:
 ; wait for VBlank
 waitVBEnd:
 ldrh r5,[r0,0x06] ; V counter
-cmp r5,0
+cmp r5,#0
 bne waitVBEnd
 waitVBStart:
 ldrh r5,[r0,0x06] ; "
-cmp r5,160
+cmp r5,#160
 bne waitVBStart
 
 ldr r5,[r0,0x0130] ; key input register
 
-ldr r6,=75840 ; 75840 = 158px * 240 * 2
+ldr r6,=#75840 ; 75840 = 158px * 240 * 2
 cmp r4,r6
 tstle r5,0x80 ; test for D-pad Down pressed
-addeq r4,r4,480 ; 480 = 1px * 240 * 2
+addeq r4,r4,#480 ; 480 = 1px * 240 * 2
 
 mov r6,0x1E0; 0x1E0 = 480 ; 480 = 1px * 240 * 2
 cmp r4,r6
 tstge r5,0x40 ; test for D-pad Up pressed
-subeq r4,r4,480 ; 480 = 1px * 240 * 2
+subeq r4,r4,#480 ; 480 = 1px * 240 * 2
 
 mov r6,0x2 ; 2 = 1px * 2
 cmp r3,r6
 tstge r5,0x20 ; test for D-pad Left pressed
-subeq r3,r3,2 ; 2 = 1px * 2
+subeq r3,r3,#2 ; 2 = 1px * 2
 
-ldr r6,=476 ; 476 = 238px * 2
+ldr r6,=#476 ; 476 = 238px * 2
 cmp r3,r6
 tstle r5,0x10 ; test for D-pad Right pressed
-addeq r3,r3,2 ; 2 = 1px * 2
+addeq r3,r3,#2 ; 2 = 1px * 2
 
 tst r5,0x01 ; test for A pressed
 mov r2,0x0000 ; set r2 to the color black
